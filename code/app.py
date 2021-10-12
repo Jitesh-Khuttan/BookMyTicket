8eccba7c9845e7b050e5f68e108014c23a50bc13
Jitesh-Khuttan <jiteshkhuttan15@gmail.com>
2021-10-12 17:34:36 +0530

    Added custom callbacks to handle JWT exceptions.

--- code/app.py
+++ code/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from code.config.settings import add_app_settings, initialize_database, initialize_jwt
+from code.config.settings import add_app_settings, initialize_database, initialize_jwt, add_jwt_callbacks
 from urls import initialize_urls
 from code.models.cinema import Cinema
 from code.models.city import City
@@ -12,6 +12,7 @@ app = add_app_settings(app)
 app = initialize_database(app)
 app = initialize_urls(app)
 jwt = initialize_jwt(app)
+jwt = add_jwt_callbacks(jwt)
 
 if __name__ == "__main__":
     app.run(port=5000)
